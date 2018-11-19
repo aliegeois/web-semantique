5d2d9ba52d4ba5679ee24a9f15ab71c4c4f3839f
aliegeois <pelanman@gmail.com>
2018-11-19 21:02:12 +0100

    update name

--- src/semantic/Main.java
+++ src/semantic/Main.java
@@ -1,4 +1,4 @@
-package websemantique;
+package semantic;
 
 import java.io.InputStream;
 
@@ -12,7 +12,7 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
 
-public class Etape2 {
+public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
